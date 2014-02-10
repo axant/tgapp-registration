d202b3915290ff5ab5703317ddda45f08d6b407f
Simone Marzola <785899+smarzola@users.noreply.github.com>
2014-02-10 15:43:33 +0100

    patch model import in model.__init__

--- registration/__init__.py
+++ registration/__init__.py
@@ -1,28 +1,30 @@
 # -*- coding: utf-8 -*-
 """The tgapp-registration package"""
 from tg import hooks, config
+from tg.configuration import milestones
 from tgext.pluggable import app_model
+from registration.model import patch_global_registration
 
 
 def plugme(app_config, options):
     app_config['_pluggable_registration_config'] = options
     if app_config.get('use_ming'):
         app_model.configure(app_config['ming_model'])
-    hooks.register('after_config', lambda app: register_dal_interface(config, app))
+    hooks.register('after_config', register_dal_interface)
+    milestones.environment_loaded.register(patch_global_registration)
     return dict(appid='registration', global_helpers=False)
 
 
-def register_dal_interface(app_config, app):
-    if app_config.get('use_sqlalchemy'):
+def register_dal_interface(app):
+    if config.get('use_sqlalchemy'):
         print 'initializing registration with sqla'
         from registration.model.sqla_models import SqlaRegistration
-        app_config['registration_dal'] = SqlaRegistration()
+        config['registration_dal'] = SqlaRegistration()
 
-    elif app_config.get('use_ming'):
+    elif config.get('use_ming'):
         print 'initializing registration with ming'
         from registration.model.ming_models import MingRegistration
-        app_config['registration_dal'] = MingRegistration()
-
+        config['registration_dal'] = MingRegistration()
     else:
         raise ValueError('registration should be used with sqlalchemy or ming')
 
