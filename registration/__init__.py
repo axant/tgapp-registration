3d57a3c8a274633b8e8e72b44db218314c3f9a44
Simone Marzola <785899+smarzola@users.noreply.github.com>
2014-02-10 15:03:19 +0100

    use config.use_ming and config.use_sqlalchemy instead of passing additional options to the pluggable

--- registration/__init__.py
+++ registration/__init__.py
@@ -6,25 +6,24 @@ from tgext.pluggable import app_model
 
 def plugme(app_config, options):
     app_config['_pluggable_registration_config'] = options
-    if app_config.get('ming_model'):
+    if app_config.get('use_ming'):
         app_model.configure(app_config['ming_model'])
-    hooks.register('after_config', lambda app: register_dal_interface(config, options, app))
+    hooks.register('after_config', lambda app: register_dal_interface(config, app))
     return dict(appid='registration', global_helpers=False)
 
 
-def register_dal_interface(app_config, options, app):
-    dal_interface = options.get('dal', 'sqla')
-    if dal_interface == 'sqla':
+def register_dal_interface(app_config, app):
+    if app_config.get('use_sqlalchemy'):
         print 'initializing registration with sqla'
         from registration.model.sqla_models import SqlaRegistration
         app_config['registration_dal'] = SqlaRegistration()
 
-    elif dal_interface == 'ming':
+    elif app_config.get('use_ming'):
         print 'initializing registration with ming'
         from registration.model.ming_models import MingRegistration
         app_config['registration_dal'] = MingRegistration()
 
     else:
-        raise ValueError('not a valid registration dal interface: %s' % dal_interface)
+        raise ValueError('registration should be used with sqlalchemy or ming')
 
     return app
